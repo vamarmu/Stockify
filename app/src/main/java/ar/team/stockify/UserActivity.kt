3e0240d2fed479f513eab81cb2270c18b38ed05f
Ch <christian@domobility.com>
2021-05-26 20:34:08 +0200

    #14 Crear pantalla de usuario

--- app/src/main/java/ar/team/stockify/UserActivity.kt
+++ app/src/main/java/ar/team/stockify/UserActivity.kt
@@ -1,7 +1,6 @@
 package ar.team.stockify
 
 import android.Manifest
-import android.R
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -12,9 +11,7 @@ import android.os.Environment
 import android.os.Handler
 import android.os.Looper
 import android.provider.MediaStore
-import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
@@ -22,57 +19,99 @@ import ar.team.stockify.databinding.ActivityUserBinding
 import ar.team.stockify.search.SearchActivity
 import java.io.File
 import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.*
 
 
 class UserActivity : AppCompatActivity(){
 
-    lateinit var currentPhotoPath: String
+    lateinit var currentPhotoName: String
     private val REQUEST_TAKE_PHOTO = 1
 
+    private lateinit var binding: ActivityUserBinding
 
-    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-        val message = if(isGranted) "PERMISSION GRANTED" else "PERMISSION REJECTED"
-        Toast.makeText(this, message, Toast.LENGTH_LONG)
+    private val requestPermissionLauncher1 = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        when {
+            isGranted -> {
+                requestPermissionLauncher2.launch(Manifest.permission.CAMERA)
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE) -> "Should show rationale"
+        }
+    }
+
+    private val requestPermissionLauncher2 = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        when {
+            isGranted -> {
+                dispatchTakePictureIntent()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> "Should show rationale"
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityUserBinding.inflate(layoutInflater)
+        binding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val sharedPref = getPreferences(Context.MODE_PRIVATE)
-        if(sharedPref.contains("username")) {
-            //val file = File(sharedPref.getString("user_image", "No image"))
-            val path = getExternalFilesDir(Environment.DIRECTORY_PICTURES)?.absolutePath.toString() + "/" + sharedPref.getString(
-                    "user_image_name",
-                    "No image"
-            )
-            val file: File = File(path)
-            Log.i("pathphotoget", path)
-            if (file.exists()) {
-                val myBitmap = BitmapFactory.decodeFile(file.getAbsolutePath())
-                binding.userImage.setImageBitmap(myBitmap)
-            }
-            binding.username.inputType = 0
-            binding.username.setText(sharedPref.getString("username", "No name"))
-            Handler(Looper.getMainLooper()).postDelayed({
-                startActivity(Intent(this, SearchActivity::class.java))
-                finish()
-            }, 3000)
+        if(hasUser()) {
+            bindUser()
         } else {
+            onClickImageButton()
+            onClickButton()
+        }
+    }
+
+    private fun onClickButton() {
+        binding.button.setOnClickListener {
+            setSharedPreference("username", binding.username.text.toString())
+            setSharedPreference("user_image", currentPhotoName)
+            bindUser()
+        }
+    }
+
+    private fun onClickImageButton() {
+        binding.imageButton.setOnClickListener {
             if (checkCamera()) {
-                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
-                requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                dispatchTakePictureIntent()
-            }
-            val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
-            with(sharedPref.edit()) {
-                putString("username", "Christian González Cañete")
-                commit()
+                requestPermissionLauncher1.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
             }
         }
+    }
+
+    private fun hasUser() =
+        existSharedPreference("user_image") && existSharedPreference("username") && File(
+            getAvatarPath()
+        ).exists()
+
+    private fun setSharedPreference(name: String, value: String) {
+        val sharedPref = getPreferences(MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putString(name, value)
+            commit()
+        }
+    }
+
+    private fun bindUser() {
+        binding.button.visibility = View.INVISIBLE
+        val file = File(getAvatarPath())
+        val myBitmap = BitmapFactory.decodeFile(file.absolutePath)
+        binding.imageButton.setImageBitmap(myBitmap)
+        binding.username.inputType = 0
+        binding.username.setText(getSharedPreference("username"))
+        startSearchActivity()
+    }
+
+    private fun startSearchActivity() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, SearchActivity::class.java))
+            finish()
+        }, 3000)
+    }
+
+    private fun getAvatarPath() =
+        getExternalFilesDir(Environment.DIRECTORY_PICTURES)?.absolutePath.toString() + "/" + getSharedPreference("user_image")
+
+    private fun existSharedPreference(name: String) = getPreferences(Context.MODE_PRIVATE).contains(name)
+
+    private fun getSharedPreference(name: String): String? {
+        return getPreferences(Context.MODE_PRIVATE).getString(name, null)
     }
 
     private fun checkCamera(): Boolean {
@@ -101,13 +140,7 @@ class UserActivity : AppCompatActivity(){
                             it
                     )
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
-                    with(sharedPref.edit()) {
-                        putString("user_image", photoURI.path)
-                        putString("user_image_name", photoURI.lastPathSegment)
-                        commit()
-                    }
-                    Log.i("pathphotosave", photoURI.path.toString())
+                    currentPhotoName = photoURI.lastPathSegment!!
                     startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
                 }
             }
@@ -117,16 +150,12 @@ class UserActivity : AppCompatActivity(){
     @Throws(IOException::class)
     private fun createImageFile(): File {
         // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
         return File.createTempFile(
-                "JPEG_${timeStamp}_", /* prefix */
+                "avatar", /* prefix */
                 ".jpg", /* suffix */
                 storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
-        }
+        )
     }
 
 }
