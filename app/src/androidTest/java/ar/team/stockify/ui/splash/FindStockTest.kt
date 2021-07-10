d345fdad9525262b447f5617685f7fd7f72ad368
Vanesa <vanessamartinezmunoz@gmail.com>
2021-07-10 10:58:14 +0200

    - Test UI HILT + Navigation

--- app/src/androidTest/java/ar/team/stockify/ui/splash/FindStockTest.kt
+++ app/src/androidTest/java/ar/team/stockify/ui/splash/FindStockTest.kt
@@ -1,28 +1,43 @@
 package ar.team.stockify.ui.splash
 
 
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.annotation.StyleRes
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.core.internal.deps.dagger.internal.Preconditions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.GrantPermissionRule
 import ar.team.stockify.R
+import ar.team.stockify.ui.HiltTestActivity
+import ar.team.stockify.ui.user.UserFragment
 import org.hamcrest.Description
 import org.hamcrest.Matcher
-import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
-import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -39,19 +54,34 @@ class FindStockTest {
             "android.permission.CAMERA",
             "android.permission.WRITE_EXTERNAL_STORAGE"
         )
+    val mockNavController = mock(NavController::class.java)
+
+
 
     @Test
     fun findStockTest() {
+
+
+        val userScenario = launchFragmentInHiltContainer<UserFragment>(
+            /* Añadir argumento para llamar UserFragment
+            bundleOf(
+                "isFromSplash" to true
+            )*/
+
+        )
+
+        /*
+        Intento de asociar el UserFragment al navigation
+        val user2Scenario =userScenario as FragmentScenario<UserFragment>
+        user2Scenario.onFragment {
+            Navigation.setViewNavController(
+                it.requireView(),
+                mockNavController
+            )
+        }*/
         val materialButton = onView(
             allOf(
                 withId(R.id.imageButton), withText("Añade tu foto"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(R.id.landing_nav_host_fragment),
-                        0
-                    ),
-                    4
-                ),
                 isDisplayed()
             )
         )
@@ -90,79 +120,18 @@ class FindStockTest {
         val materialButton2 = onView(
             allOf(
                 withId(R.id.button), withText("Guardar"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(R.id.landing_nav_host_fragment),
-                        0
-                    ),
-                    7
-                ),
                 isDisplayed()
             )
         )
+
         materialButton2.perform(click())
 
-        val appCompatImageView = onView(
-            allOf(
-                withId(R.id.search_button), withContentDescription("Buscar"),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.search_bar),
-                        childAtPosition(
-                            withId(R.id.search_view),
-                            0
-                        )
-                    ),
-                    1
-                ),
-                isDisplayed()
-            )
-        )
-        appCompatImageView.perform(click())
+        // Verificar que navega al MainContentFragment
+        verify(mockNavController).navigate(R.id.mainContentFragment)
 
-        val searchAutoComplete = onView(
-            allOf(
-                withId(R.id.search_src_text),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.search_plate),
-                        childAtPosition(
-                            withId(R.id.search_edit_frame),
-                            1
-                        )
-                    ),
-                    0
-                ),
-                isDisplayed()
-            )
-        )
-        searchAutoComplete.perform(replaceText("msft"), closeSoftKeyboard())
-
-        val recyclerView = onView(
-            allOf(
-                withId(R.id.recyclerview),
-                childAtPosition(
-                    withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
-                    1
-                )
-            )
-        )
-        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
-
-        val textView = onView(
-            allOf(
-                withId(R.id.tDetails_company_name), withText("Microsoft Corporation"),
-                withParent(
-                    allOf(
-                        withId(R.id.details_header),
-                        withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))
-                    )
-                ),
-                isDisplayed()
-            )
-        )
-        textView.check(matches(withText("Microsoft Corporation")))
     }
+
+
 
     private fun childAtPosition(
         parentMatcher: Matcher<View>, position: Int
@@ -179,6 +148,56 @@ class FindStockTest {
                 return parent is ViewGroup && parentMatcher.matches(parent)
                         && view == parent.getChildAt(position)
             }
+        }
+    }
+
+    /**
+     * launchFragmentInContainer from the androidx.fragment:fragment-testing library
+     * is NOT possible to use right now as it uses a hardcoded Activity under the hood
+     * (i.e. [EmptyFragmentActivity]) which is not annotated with @AndroidEntryPoint.
+     *
+     * As a workaround, use this function that is equivalent. It requires you to add
+     * [HiltTestActivity] in the debug folder and include it in the debug AndroidManifest.xml file
+     * as can be found in this project.
+     */
+    inline fun <reified T : Fragment> launchFragmentInHiltContainer(
+        fragmentArgs: Bundle? = null,
+        @StyleRes themeResId: Int = R.style.FragmentScenarioEmptyFragmentActivityTheme,
+        crossinline action: Fragment.() -> Unit = {}
+    ) {
+        val startActivityIntent = Intent.makeMainActivity(
+            ComponentName(
+                ApplicationProvider.getApplicationContext(),
+                HiltTestActivity::class.java
+            )
+        ).putExtra(
+            "androidx.fragment.app.testing.FragmentScenario.EmptyFragmentActivity.THEME_EXTRAS_BUNDLE_KEY",
+            themeResId
+        )
+
+        ActivityScenario.launch<HiltTestActivity>(startActivityIntent).onActivity { activity ->
+            val fragment: Fragment = activity.supportFragmentManager.fragmentFactory.instantiate(
+                Preconditions.checkNotNull(T::class.java.classLoader),
+                T::class.java.name
+            )
+
+            //Intento de dar tiempo a que cargue la activity
+            //Thread.sleep(5000)
+
+            // Intento de asociar navigation con Activity
+                /*Navigation.setViewNavController(
+                    fragment.requireView(),
+                    mockNavController
+                )*/
+
+
+            fragment.arguments = fragmentArgs
+            activity.supportFragmentManager
+                .beginTransaction()
+                .add(android.R.id.content, fragment, "")
+                .commitNow()
+
+            fragment.action()
         }
     }
 }
---
+++ app/src/main/java/ar/team/stockify/ui/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package ar.team.stockify.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
